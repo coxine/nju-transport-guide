912335cc5c6fe1b19c2bf227cfa0cc77610e242a
coxine <59347549+coxine@users.noreply.github.com>
2026-08-16 17:17:27 +0800

    feat: update direction details for multiple routes to remove redundant information

--- src/data/routes/to-hub/gulou-nanjing-south.ts
+++ src/data/routes/to-hub/gulou-nanjing-south.ts
@@ -17,7 +17,7 @@ export const gulouNanjingSouth: RouteEntry = {
           mode: "metro",
           duration: "21-25",
           stopCount: "9-10",
-          direction: "八卦洲大桥南/河定桥",
+          direction: "中国药科大学/河定桥",
           lineName: "1号线",
           lineKey: "nj-line-1",
           firstDeparture: "05:53",
